d9dcd690913dd14edfb29c2a7b8342a6c95bc54f
Zabcode <hunter.eal@yandex.ru>
2019-07-11 00:19:46 +0300

    setting up api router

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
 import Koa from 'koa';
 
 import bodyParser from 'koa-body';
+import { api } from './api';
 
 export const app = new Koa();
 
 app.use(bodyParser());
+app.use(api.routes()).use(api.allowedMethods());
