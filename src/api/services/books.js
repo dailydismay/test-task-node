45e307bb852febda99567ea28ca2902cdce7b6f9
Zabcode <hunter.eal@yandex.ru>
2019-07-11 00:37:00 +0300

    updated books service

--- src/api/services/books.js
+++ src/api/services/books.js
@@ -1,5 +1,6 @@
 import { query } from '../../db';
 import { execOrderBy } from '../../util';
+import { execWhere } from '../../util/where';
 
 export const create = async data => {
   const [book] = await query(
@@ -31,12 +32,19 @@ export const show = async id => {
   return book;
 };
 
-export const list = async ({ offset = 0, limit = 100, orderBy, sort }) => {
+export const list = async ({
+  offset = 0,
+  limit = 100,
+  orderBy,
+  sort,
+  author,
+}) => {
   const baseQuery = `
         SELECT book.id, book.title, book.description, book.date, image.*, author.* FROM books book
         JOIN images image ON image.id = book.image
         JOIN authors author ON author.id = book.author
         ${execOrderBy('book', orderBy, ['id', 'title', 'date'], sort)}
+        ${execWhere('book', 'author', author)}
         LIMIT ${limit}
         OFFSET ${offset}
       `;
