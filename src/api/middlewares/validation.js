73136a0f88fd5376e10146aefbc912a5e7737273
Ilya <44939563+zabozlaev@users.noreply.github.com>
2020-01-15 17:04:24 +0300

    Update validation.js

--- src/api/middlewares/validation.js
+++ src/api/middlewares/validation.js
@@ -2,7 +2,7 @@ import { validateSchema } from '../../util';
 
 export const validationMiddleware = (schema, key) => async (ctx, next) => {
   try {
-    await validateSchema(ctx[key], schema);
+    await validateSchema(key.split('.').reduce((curr, prev) => prev[curr], ctx), schema);
     await next();
   } catch (error) {
     ctx.status = 422;
